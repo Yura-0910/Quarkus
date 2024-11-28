46c1d27c550e36d3fe4cb5cbd46eabd74417c556
Yura Mazurov <mazurovyura09@yandex.ru>
2024-11-28 14:43:57 +0300

    Тест на:: разрешен запрос с ролью User

--- src/test/java/ru/lainer/JwtTests.java
+++ src/test/java/ru/lainer/JwtTests.java
@@ -1,6 +1,8 @@
 package ru.lainer;
 
 import io.quarkus.test.junit.QuarkusTest;
+import io.smallrye.jwt.build.Jwt;
+import org.eclipse.microprofile.jwt.Claims;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -27,5 +29,28 @@ public class JwtTests {
     response.then()
         .statusCode(200)
         .body(containsString("hello anonymous, authScheme: null"));
+  }
+
+  @Test
+  @Order(2)
+  void testUserRoleIsAllowed(){
+    Response response = given().auth()
+        .oauth2(generateValidUserToken())
+        .when()
+        .get("/secured/roles-allowed").andReturn();
+
+    response.then()
+        .statusCode(200)
+        .body(containsString(
+            "hello test_user@yandex.ru, authScheme: Bearer, nickname(из токена): test_source"));
+  }
+
+  String generateValidUserToken(){
+    return Jwt
+        .issuer("https://lainer.ru/issuer")
+        .upn("test_user@yandex.ru")
+        .groups("User")
+        .claim(Claims.nickname, "test_source")
+        .sign();
   }
 }
